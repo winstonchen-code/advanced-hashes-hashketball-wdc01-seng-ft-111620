98a4bd80e82f5d2c8a9f32ae852ad2c989979d40
Winston Chen <winstonchen.wm@gmail.com>
2020-11-07 21:17:20 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -181,7 +181,7 @@ def player_stats(player_n)
   game_hash.each do |home_away, keys|
     keys[:players].each do |player|
       if player[:player_name] == player_n
-        return player.delete_if { |stat, value| [:player_name].include?(stat)}
+        return player
       end
     end
   end
