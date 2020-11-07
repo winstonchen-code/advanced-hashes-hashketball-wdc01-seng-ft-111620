5aff2ff09a1c5ad224513e6e88c072fbf0516a42
Winston Chen <winstonchen.wm@gmail.com>
2020-11-07 21:14:29 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -129,7 +129,13 @@ end
 # Write code here
 
 def num_points_scored(player_search)
-  get_player_by_name(player_search)[:points]
+  game_hash.each do |team, team_info|
+    team_info[:players].each do |player|
+      if player[:player_name] == player_search
+        return player[:points]
+      end
+    end
+  end
 end
 
 def shoe_size(name)
